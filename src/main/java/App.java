67ec75f0f638477b65d474cf1f7c00046f3b5724
Mauricio Matamala <mauricio.politecnico@gmail.com>
2020-04-08 10:06:51 +0200

    inicio

---
+++ src/main/java/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        Copyright c = new Copyright();
+        c.getText();
+    }
+}
---
+++ src/main/java/Copyright.java
@@ -0,0 +1,10 @@
+public class Copyright {
+    public void getText(){
+        System.out.println("Copyright (C) 1883 Jesus D. Bueno - All Rights Reserved " +
+                        "\nYou may use, distribute and modify this code under the" +
+                        "\nterms of the XYZ license, which unfortunately won't be" +
+                        "\nwritten for another century." +
+                "\n\nYou should have received a copy of the XYZ license with " +
+                "\nthis file. If not, please visit : http://www.jesus_d.com");
+    }
+}
